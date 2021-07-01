8db8cf991792e873bf56d89a82937ef4e359ab57
alexander <zeimedee@gmail.com>
2021-07-01 11:57:22 +0000

    :rocket: chanels-direction

--- demo/chanels.go
+++ demo/chanels.go
@@ -13,10 +13,12 @@ func sum(s []int, c chan int) {
 func main() {
 	nums := []int{1, 3}
 	nums2 := []int{2, 4}
+	nums3 := []int{5, 3}
 	c := make(chan int)
+	go sum(nums3, c)
 	go sum(nums2, c)
 	go sum(nums, c)
-	v, b := <-c, <-c
-	fmt.Printf("chanel value: a:%d and b:%d", v, b)
+	v, b, n := <-c, <-c, <-c
+	fmt.Printf("chanel value: a:%d and b:%d and c:%d,\n total = %d", v, b, n, v+b)
 
 }
