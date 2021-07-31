57601ca04d06a8817fe411c377505813c17f0db6
alexander <zeimedee@gmail.com>
2021-07-31 21:27:55 +0000

    :rocket: map

--- demo/maps.go
+++ demo/maps.go
@@ -13,6 +13,11 @@ func main() {
 	fmt.Println("length", len(m))
 
 	n := map[string]int{"a": 2, "b": 3, "c": 4}
+	//check if element exist, map returns 2 items
+	k, ok := n["a"]
+	if ok {
+		fmt.Println(k)
+	}
 
 	fmt.Println("n:a", n["a"])
 }
